d3317da6315bd17b82b019a20c42d9cd5c6827e5
gdenis <germain.denis@yahooinc.com>
2025-10-17 21:54:45 +0800

    add import

--- k6/load-test.js
+++ k6/load-test.js
@@ -1,6 +1,7 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
-import { Rate } from 'k6/metrics';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
 
 // Custom metrics
 const errorRate = new Rate('errors');
